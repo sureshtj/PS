5c5ec9fcc96f378d2e5b39c187413bf933bdb910
sureshtj <32847424+sureshtj@users.noreply.github.com>
2018-05-24 16:50:26 +0530

    test

--- PS/testngBatchFile.bat
+++ PS/testngBatchFile.bat
@@ -1,5 +1,5 @@
-set projectPath=D:\Git\PS
+set projectPath=D:\Selenium_New\PS
 cd\
 cd %projectPath%
-set classpath=%projectPath%\bin;%projectPath%\jar\*;
+set classpath=%projectPath%\bin;%projectPath%\lib\*;
 java org.testng.TestNG testng.xml
