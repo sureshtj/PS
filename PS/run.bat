83c303212f96dc8784125116fb2e33a40016a921
sureshtj <32847424+sureshtj@users.noreply.github.com>
2018-05-27 00:24:07 +0530

    Update run.bat

--- PS/run.bat
+++ PS/run.bat
@@ -1,5 +1,5 @@
 set projectLocation=C:\jenkins\workspace\suresh\PS
-%projectLocation%
+cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*;D:\jenkins\PS\testng\*
 java org.testng.TestNG %projectLocation%\testng.xml
 pause
