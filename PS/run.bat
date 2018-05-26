ad547ac8abb671a55584e94496ed1abaa2c17731
sureshtj <32847424+sureshtj@users.noreply.github.com>
2018-05-26 23:46:55 +0530

    Update run.bat

--- PS/run.bat
+++ PS/run.bat
@@ -1,5 +1,5 @@
-set projectPath=C:\jenkins\workspace\project\PS
-cd\
-cd %projectPath%
-set classpath=%projectPath%\bin;%projectPath%\lib\*;
-java -cp "C:\jenkins\workspace\project\PS\lib\testng-6.0.jar" org.testng.TestNG testng.xml
+set projectLocation=D:\jenkins\PS
+%projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*;D:\jenkins\PS\testng\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
